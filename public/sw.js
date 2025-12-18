b1177b6ceed98d324644289f6d578c90ac22987c
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-12-18 16:08:23 +0630

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,20 @@
-const CACHE_NAME = 'clipboard-manager-cache-v1';
+const CACHE_NAME = 'clipboard-manager-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/dist/output.css',
+  '/clipboard-manager/',
+  '/clipboard-manager/index.html',
+  '/clipboard-manager/manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
   );
 });
