4618ebff4d9dfdafcf1bd85a606704333a513ee2
kokhinmaungwin <khinmaungwin675@gmail.com>
2025-12-18 22:20:54 +0630

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+let deferredPrompt;
+const installBtn = document.getElementById("installBtn");
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.hidden = false;
+});
+
+installBtn.addEventListener("click", async () => {
+  if (!deferredPrompt) return;
+
+  deferredPrompt.prompt();
+  const { outcome } = await deferredPrompt.userChoice;
+
+  if (outcome === "accepted") {
+    console.log("User accepted the install prompt");
+  } else {
+    console.log("User dismissed the install prompt");
+  }
+
+  deferredPrompt = null;
+  installBtn.hidden = true;
+});
